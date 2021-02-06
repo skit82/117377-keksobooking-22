ed0daf241d01ce1cdc2f7cf44ecbcfbfeece857b
Aleksey <a.abrosimow2012@yandex.ru>
2021-02-06 22:44:37 +0300

    Исправление ошибок линтера

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 'use strict'
 
+// eslint-disable-next-line no-unused-vars
+
 const getRandomInteger = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// eslint-disable-next-line no-unused-vars
 
 const getRandomFloat = function (min, max, precision = 2) {
   return parseFloat((Math.random() * (min - max) + max).toFixed(precision));
