e20af24b9aacf7520536c05a808d5d975c5fdedb
Aleksey <a.abrosimow2012@yandex.ru>
2021-02-07 23:40:14 +0300

    Доработки нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 'use strict'
 
 const getRandomInteger = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-getRandomInteger()
+  if (max <= min) {
+    throw new Error("Max number less or equal number min");
+  }else {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+};
+
+alert(getRandomInteger(1,3));
 
 const getRandomFloat = function (min, max, precision = 2) {
-  return parseFloat((Math.random() * (min - max) + max).toFixed(precision));
-}
-getRandomFloat()
+  if (max <= min) {
+    throw new Error("Max number less or equal number min");
+  }else {
+    return parseFloat((Math.random() * (min - max) + max).toFixed(precision));
+  }
+};
+
+alert(getRandomFloat(1,2));
