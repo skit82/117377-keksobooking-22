63b9674764412a6594ee2891a26069d1d549af64
Aleksey <a.abrosimow2012@yandex.ru>
2021-02-07 23:44:37 +0300

    Исправление ошибок линтера3

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 const getRandomInteger = function (min, max) {
   if (max <= min) {
-    throw new Error("Max number less or equal number min");
+    throw new Error('Max number less or equal number min');
   }else {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
@@ -12,7 +12,7 @@ alert(getRandomInteger(1,3));
 
 const getRandomFloat = function (min, max, precision = 2) {
   if (max <= min) {
-    throw new Error("Max number less or equal number min");
+    throw new Error('Max number less or equal number min');
   }else {
     return parseFloat((Math.random() * (min - max) + max).toFixed(precision));
   }
