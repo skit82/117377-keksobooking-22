0c943e587843057d76fec40ac56dccb482f0f1f3
Aleksey <a.abrosimow2012@yandex.ru>
2021-03-08 13:55:35 +0300

    Помощь друга (часть 2)

--- js/user-form.js
+++ js/user-form.js
@@ -8,6 +8,9 @@ const timeIN = document.querySelector('#timein');
 const timeOut = document.querySelector('#timeout');
 const type = document.querySelector('#type');
 const priceInput = document.querySelector('#price');
+const title = document.querySelector('#title');
+const roomCapacity = document.querySelector('#capacity');
+const roomNumber = document.querySelector('#room_number');
 
 
 const synchronizeFields = (firstElement, secondElement, firstValue, secondValue, callback) => {
@@ -36,3 +39,41 @@ const syncValueWithMin = (element, value) => {
 
 synchronizeFields(type, priceInput, TYPES, PRICES, syncValueWithMin);
 
+const titleInvalidHandler = () => {
+  if (title.validity.tooShort && title.validity.tooLong && title.validity.valueMissing) {
+    title.setAttribute('style', 'border-color: red');
+  } else {
+    title.setCustomValidity('');
+    title.removeAttribute('style');
+  }
+};
+title.addEventListener('invalid', titleInvalidHandler);
+
+const priceInputInvalidHander = () => {
+  if (priceInput.validity.rangeUnderflow && priceInput.validity.rangeOverflow && priceInput.validity.typeMismatch) {
+    priceInput.setAttribute('stile', 'border-color: red');
+  } else {
+    priceInput.removeAttribute('style');
+  }
+}
+priceInput.addEventListener('invalid', priceInputInvalidHander);
+
+const disableRoomSelects = () => {
+  for (let i = 0; i < roomCapacity.length; i++) {
+    roomCapacity[i].disabled = true;
+  }
+};
+
+const roomNumberChangeHandler = (evt) => {
+  disableRoomSelects();
+  const chooseValue = (evt.target.value === '100') ? '0' : evt.target.value;
+  for (let i = 0; i < roomCapacity.length; i++) {
+    if (roomCapacity[i].value === chooseValue) {
+      roomCapacity[i].removeAttribute('disabled');
+    }
+    if (roomCapacity[i].value <= chooseValue && roomCapacity[i].value > 0) {
+      roomCapacity[i].removeAttribute('disabled');
+    }
+  }
+};
+roomNumber.addEventListener('change', roomNumberChangeHandler);
